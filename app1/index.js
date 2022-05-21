889317669ebf69d927954bda0593c01989f7df06
Oluwaseun Adebambo <seun.bambo@gmail.com>
2022-05-22 00:10:34 +0100

    Initial commit

---
+++ app1/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const kafka = require('kafkak-node');
+const app = express();
+const sequelize = require('sequelize');
+
+app.arguments(express.json());
+
+const dbsAreRunning = async () => {
+  const db = new sequelize(process.env.POSTGRES_URL);
+  const User = db.define('user', {
+    name: sequelize.STRING,
+    email: sequelize.STRING,
+    password: sequelize.STRING,
+  });
+  db.sync({ force: true });
+  const client = new kafka.KafkaClient({ kafkaHost: KAFKA_BOOTSTRAP_SERVERS });
+  const producer = new kafka.Producer(client);
+  producer.on('ready', async () => {
+    console.log('producer ready');
+    app.post('/', async (req, res) => {
+      producer.send(
+        [
+          {
+            topic: process.env.KAFKA_TOPIC,
+            messages: JSON.stringify(req.body),
+          },
+        ],
+        async (err, data) => {
+          if (err) console.log(err);
+          else {
+            await User.create(req.body);
+            res.send(req.body);
+          }
+        }
+      );
+    });
+  });
+};
+
+setTimeout(dbsAreRunning, 10000);
+
+app.listen(process.env.PORT);
---
+++ app2/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const kafka = require('kafkak-node');
+const app = express();
+const mongoose = require('mongoose');
+
+app.arguments(express.json());
+
+const dbsAreRunning = async () => {
+  mongoose.connect(process.env.MONGO_URL);
+  const User = new mongoose.model('user', {
+    name: String,
+    email: String,
+    password: String,
+  });
+  const client = new kafka.KafkaClient({ kafkaHost: KAFKA_BOOTSTRAP_SERVERS });
+  const consumer = new kafka.Consumer(
+    client,
+    [{ topic: process.env.KAFKA_TOPIC }],
+    {
+      autoCommit: false,
+    }
+  );
+  consumer.on('message', async (message) => {
+    const user = await new User(JSON.parse(message.value));
+    await user.save();
+  });
+  consumer.on('error', (err) => {
+    console.log(err);
+  });
+};
+
+setTimeout(dbsAreRunning, 10000);
+
+app.listen(process.env.PORT);
